4ffa66672a06aade68774181b095076f7adf1da5
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2026-05-02 15:28:02 +0530

    Creational Design Patterns Blue Coloured
    Creational Design Patterns Blue Coloured

--- creational/start.go
+++ creational/start.go
@@ -3,9 +3,21 @@ package creational
 import "fmt"
 
 func StartCreational() {
-	fmt.Println("Creational Design Patterns")
+	blue := "\033[1;34m"
+	reset := "\033[0m"
+
+	fmt.Println(blue + "Creational Design Patterns" + reset)
 	fmt.Println()
 
-	fmt.Println("Singleton Design Pattern => Single Instance, Thread-Safe, Lazy-Initialization")
+	fmt.Println(blue + "[1] SINGLETON DESIGN PATTERN" + reset)
+	fmt.Println("    Description: Single Instance, Thread-Safe, Lazy-Initialization")
+	fmt.Println()
 	MySingleton()
+
+	fmt.Println()
+	fmt.Println(blue + "[2] FACTORY METHOD DESIGN PATTERN" + reset)
+	fmt.Println("    Description: Define Interface For Creating Objects")
+	fmt.Println()
+	MyFactoryMethod()
+	fmt.Println()
 }
